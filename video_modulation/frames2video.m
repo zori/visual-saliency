0d5347ec422ccf9d0fda530ff72342d66a11c0eb
Zori Kostadinova <zornitsa.kostadinova.bg@gmail.com>
2015-05-13 08:30:57 +0900

    Transforming individual frames to video.

--- video_modulation/frames2video.m
+++ video_modulation/frames2video.m
@@ -1,18 +1,8 @@
-video_sequence = 'etdb_foreman_352x288_30'; % 'bridge_close'
-video_folder = fullfile('dataset', 'video', video_sequence);
-img_extension = 'png';
-video_extension = 'avi';
-n_frames = 300;
-
-imgs = dir(fullfile(video_folder, filesep, ['*.' img_extension]));
-
-output_video = VideoWriter([video_folder, '.', video_extension]);
-output_video.FrameRate = 30;
-open(output_video);
-
-for k = 1:n_frames
-    img = imread(fullfile(video_folder, imgs(k).name));
-    writeVideo(output_video, img);
+function frames2video()
+video_sequence=video_dataset_data();
+for k=1:length(video_sequence)
+    video_folder = fullfile('dataset', 'video', [video_sequence(k).name '_' int2str(video_sequence(k).n_frames)]);
+    single_frames2video(video_folder, video_sequence(k).n_frames);
 end
 
-close(output_video);
+end
---
+++ video_modulation/single_frames2video.m
@@ -0,0 +1,23 @@
+% Zornitsa Kostadinova
+% May 2015
+% 8.5.0.197613 (R2015a)
+function single_frames2video(video_folder, n_frames)
+% for example:
+% video_folder = 'dataset/video/orig_city_300'
+% n_frames = 300
+img_extension = 'png';
+video_extension = 'avi';
+
+imgs = dir(fullfile(video_folder, filesep, ['*.' img_extension]));
+
+output_video = VideoWriter([video_folder, '.', video_extension]);
+output_video.FrameRate = 30;
+open(output_video);
+
+for k = 1:n_frames
+    img = imread(fullfile(video_folder, imgs(k).name));
+    writeVideo(output_video, img);
+end
+
+close(output_video);
+end
