3118c911aa9e3242cce52a2206c3b6d85e30fe93
Zori Kostadinova <zornitsa.kostadinova.bg@gmail.com>
2015-06-04 12:41:33 +0900

    Fixed: the alpha parameter is set only in case the modulation type is exponential smoothing. Added a function to hold the constant variable MOD_TYPE_EXP_SMOOTHING (as prescribed by the MATLAB blog; weird solution to lack of constants in the language).

---
+++ video_modulation/functions/MOD_TYPE_EXP_SMOOTHING.m
@@ -0,0 +1,6 @@
+% Zornitsa Kostadinova
+% Jun 2015
+% 8.5.0.197613 (R2015a)
+function mt = MOD_TYPE_EXP_SMOOTHING
+mt=3;
+end
--- video_modulation/tldSaliency.m
+++ video_modulation/tldSaliency.m
@@ -4,8 +4,6 @@
 %
 
 function [bb,conf,weights] = tldSaliency(opt)
-fprintf('\nmodulation type: %s\n', int2str(opt.modulation));
-
 global tld; % holds results and temporal variables
 % allow to not visualise the modulation
 % however, interrupting the processing earlier (by pressing 'q') depends on
@@ -17,6 +15,9 @@ TO_VISUALIZE = true;
 %
 global param;
 param = {};
+param.modulation_type = opt.modulation;
+modulation_type_str = int2str(param.modulation_type);
+fprintf('\nmodulation type: %s\n', modulation_type_str);
 %---SALIENCY-END---
 %%
 
@@ -70,23 +71,6 @@ end
 % TODO(zori) where is the "magical 6" taken from
 param.MAGICAL_DIMENSION = 6;
 W = zeros(param.nEhcMaps, param.MAGICAL_DIMENSION, param.wSpan);
-smoothing_param = 1 / opt.smoothing_param_recipr; % exponential smoothing param; in (0,1]
-fprintf('\nsmoothing param recipr: %s; smoothing param: %f\n', num2str(opt.smoothing_param_recipr), smoothing_param);
-alpha = ones(param.wSpan,1) .* smoothing_param;
-for k = 2:param.wSpan
-    alpha(k:end) = alpha(k:end) .* (1 - smoothing_param);
-end
-% for smoothing_param = 1/3:
-% alpha: 0.3333    0.2222    0.1481    0.0988    0.0658    0.0439    0.0293    0.0195    0.0130
-%
-% for smoothing_param = 1/6:
-% alpha: 0.1667    0.1389    0.1157    0.0965    0.0804    0.0670    0.0558    0.0465    0.0388
-%
-% for smoothing_param = 1/7:
-% alpha: 0.1429    0.1224    0.1050    0.0900    0.0771    0.0661    0.0567    0.0486    0.0416
-%
-% for smoothing_param = 1/9:
-% 0.1111    0.0988    0.0878    0.0780    0.0694    0.0617    0.0548    0.0487    0.0433
 
 flash = {};
 flash.frm   = cell(param.flashL, 1);
@@ -108,8 +92,26 @@ if ~isempty(opt.source) && isfield(opt.source,'init_bb_name')
 else
     video_name_append = '';
 end
-video_name_append = [video_name_append ' MOD' num2str(opt.modulation)];
-if opt.modulation == 3
+video_name_append = [video_name_append ' MOD' modulation_type_str];
+if param.modulation_type == MOD_TYPE_EXP_SMOOTHING
+    param.smoothing_param = 1 / opt.smoothing_param_recipr; % exponential smoothing param; in (0,1]
+    fprintf('\nsmoothing param recipr: %s; smoothing param: %f\n', num2str(opt.smoothing_param_recipr), param.smoothing_param);
+    param.alpha = ones(param.wSpan,1) .* param.smoothing_param;
+    for k = 2:param.wSpan
+        param.alpha(k:end) = param.alpha(k:end) .* (1 - param.smoothing_param);
+    end
+    % for alpha.smoothing_param = 1/3:
+    % alpha: 0.3333    0.2222    0.1481    0.0988    0.0658    0.0439    0.0293    0.0195    0.0130
+    %
+    % for alpha.smoothing_param = 1/6:
+    % alpha: 0.1667    0.1389    0.1157    0.0965    0.0804    0.0670    0.0558    0.0465    0.0388
+    %
+    % for alpha.smoothing_param = 1/7:
+    % alpha: 0.1429    0.1224    0.1050    0.0900    0.0771    0.0661    0.0567    0.0486    0.0416
+    %
+    % for alpha.smoothing_param = 1/9:
+    % 0.1111    0.0988    0.0878    0.0780    0.0694    0.0617    0.0548    0.0487    0.0433
+    
     video_name_append = [video_name_append ' SMOOTH' num2str(opt.smoothing_param_recipr)];
 end
 output_video_name = [timestamp video_name_append];
@@ -144,8 +146,7 @@ diffs = make_diffs(pyrasBef);
 % make mask
 % SBef = simple_n(enlarge(get_salimap(pyrasBef)));
 [SBef, S_flicker] = get_salimap(pyrasBef);
-SBef = simple_n(enlarge(SBef));
-S_flicker = simple_n(enlarge(S_flicker));
+[SBef, S_flicker] = process_saliency(SBef, S_flicker);
 % fprintf('#1 mean saliency (normalised, upscaled):%f\n',mean(SBef(:)));
 % fprintf('#1 mean flicker saliency (as is):%f\n',mean(S_flicker(:)));
 avg_flicker_saliency=zeros(length(tld.source.idx),1);
@@ -162,6 +163,7 @@ mask = simple_n(get_video_mask(curBB) .* SBef);
 % preparation for next frame
 % re-generation of pyras
 pyrasAft = make_pyras(editedFrame);
+pyras_modulated = pyrasAft;
 % calculate gamma
 rgbDiff = abs(editedFrame - curFrame);
 gamma = (1 - mean(rgbDiff(:)));
@@ -195,13 +197,12 @@ for i = 2:length(tld.source.idx) % for every frame
         % make mask
         % SBef = simple_n(enlarge(get_salimap(pyrasBef)));
         [SBef, S_flicker] = get_salimap(pyrasBef); % TODO(zori) this is flicker BEFORE; would be the same regardless of the modulation type
-        SBef = simple_n(enlarge(SBef));
-        S_flicker = simple_n(enlarge(S_flicker));
+        [SBef, S_flicker] = process_saliency(SBef, S_flicker);
         %         fprintf('#2 mean saliency (normalised, upscaled):%f\n',mean(SBef(:)));
         %         fprintf('#2 mean flicker saliency (as is):%f\n',mean(S_flicker(:)));
         avg_flicker_saliency(i)=mean(S_flicker(:));
         mask = simple_n(get_video_mask(curBB) .* SBef);
-        
+
         % do enhancement
         [editedFrame, W(:,:,1)] = ...
             modu_frame(curFrame, diffs, mask, W(:,:,2)*gamma, lastPyrasAft);
@@ -220,7 +221,7 @@ for i = 2:length(tld.source.idx) % for every frame
     end
     
     if i == param.flashL
-        [input_img, output_img, meanW, weights, weightsIdx] = modulate_frame(opt.modulation, flash, W, weights, weightsIdx);
+        [input_img, output_img, meanW, weights, weightsIdx] = modulate_frame(flash, W, weights, weightsIdx);
 
         if TO_VISUALIZE
             visualHandles = init_visual(fig_h, input_img, output_img, [], [], meanW, ...
@@ -230,13 +231,16 @@ for i = 2:length(tld.source.idx) % for every frame
         
         
     elseif i > param.flashL
-        [input_img, output_img, meanW, weights, weightsIdx] = modulate_frame(opt.modulation, flash, W, weights, weightsIdx);
+        [input_img, output_img, meanW, weights, weightsIdx] = modulate_frame(flash, W, weights, weightsIdx);
 
         if TO_VISUALIZE
             visualHandles = visualize(fig_h, input_img, output_img, [], [], meanW, ...
                 flash.curBB{1}, visualHandles);
         end
-        write_videos(video_writers, output_img, SBef, S_flicker);
+        pyras_modulated = make_pyras(output_img, pyras_modulated);
+        [s_modulated, s_flicker_modulated] = get_salimap(pyras_modulated); % TODO(zori) this is flicker BEFORE; would be the same regardless of the modulation type
+        [s_modulated, s_flicker_modulated] = process_saliency(s_modulated, s_flicker_modulated);
+        write_videos(video_writers, output_img, s_modulated, s_flicker_modulated);
     end
     
     %---SALIENCY-END---
@@ -274,13 +278,16 @@ end
 for i = 1:param.flashL
     [flash, W] = shift_storage(flash, W);
     
-    [input_img, output_img, meanW, weights, weightsIdx] = modulate_frame(opt.modulation, flash, W, weights, weightsIdx);
+    [input_img, output_img, meanW, weights, weightsIdx] = modulate_frame(flash, W, weights, weightsIdx);
 
     if TO_VISUALIZE
         visualHandles = visualize(fig_h, input_img, output_img, [], [], meanW, ...
             flash.curBB{1}, visualHandles);
     end
-    write_videos(video_writers, output_img, SBef, S_flicker);
+    pyras_modulated = make_pyras(output_img, pyras_modulated);
+    [s_modulated, s_flicker_modulated] = get_salimap(pyras_modulated); % TODO(zori) this is flicker BEFORE; would be the same regardless of the modulation type
+    [s_modulated, s_flicker_modulated] = process_saliency(s_modulated, s_flicker_modulated);
+    write_videos(video_writers, output_img, s_modulated, s_flicker_modulated);
 end
 
 on_finish(video_writers);
@@ -311,6 +318,14 @@ open(video_writer_object);
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function varargout = process_saliency(varargin)
+varargout = cell(size(varargin));
+for k=1:nargin
+    varargout{k} = simple_n(enlarge(varargin{k}));
+end
+end
+        
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [flash, W] = store_and_shift(flash, W, curFrame, diffs, mask, curBB)
 % flash storage
 flash.frm{1}   = curFrame;
@@ -331,10 +346,10 @@ flash.curBB = circshift(flash.curBB, 1);
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [input_img, output_img, meanW, weights, weightsIdx] = modulate_frame(modulation_type, flash, W, weights, weightsIdx)
+function [input_img, output_img, meanW, weights, weightsIdx] = modulate_frame(flash, W, weights, weightsIdx)
 global param;
 input_img = flash.frm{1};
-switch modulation_type
+switch param.modulation_type
     case 1
         meanW = W(:,:,2); % just get the last weights, no averaging; will result in flicker
     case 2
@@ -342,12 +357,13 @@ switch modulation_type
         % or, equivalently:
         % same as case 3, only alpha is a vector of length param.wSpan
         % with values 1/(param.wSpan)
-    case 3
-        ww = reshape(W, param.nEhcMaps * param.MAGICAL_DIMENSION, param.wSpan);
+    case MOD_TYPE_EXP_SMOOTHING
+        W_reshaped = reshape(W, param.nEhcMaps * param.MAGICAL_DIMENSION, param.wSpan);
         % TODO(zori) get the alpha, make sure it is set only once, and the
         % smoothing param is available for the name of the video in the
         % beginning
-        res = ww * alpha;
+        assert(isfield(param, 'alpha'));
+        res = W_reshaped * param.alpha;
         meanW = reshape(res, param.nEhcMaps, param.MAGICAL_DIMENSION);
     otherwise
         error('wrong modulation value');
