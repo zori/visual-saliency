d86e2121fcfb0b1643cc94cb750d60171e87a031
Zori Kostadinova <zornitsa.kostadinova.bg@gmail.com>
2015-06-08 18:17:45 +0900

    Major refactoring. Make it possible to save statistics and videos from the original, unmodulated video too.

--- video_modulation/tldSaliency.m
+++ video_modulation/tldSaliency.m
@@ -116,25 +116,44 @@ if param.modulation_type == MOD_TYPE_EXP_SMOOTHING
 end
 output_video_name = [timestamp video_name_append];
 output_video_path = fullfile('..', 'experiments', opt.sequence_name);
-if ~exist(output_video_path,'dir')
+if ~exist(output_video_path, 'dir')
     mkdir(output_video_path)
 end
-% output_flicker_saliency_txt = fullfile(output_video_path, [output_video_name ' flicker-saliency' '.txt']);
+saliency_flicker_writers{1}.txt = fullfile(output_video_path, [output_video_name ' saliency-flicker' '.txt']);
+saliency_flicker_writers{2}.txt = fullfile(output_video_path, ['saliency-flicker' '.txt']);
 
 % create video writer objects for the video, the two saliency videos and the
 % merged output (the three next to each other)
 
-global n_output_videos; % to be verified in write_videos()
-n_output_videos = 4;
-video_writers = cell(1, n_output_videos);
+% check whether the original video's statistic (saliency, flicker saliency,
+% flicker saliency file summary) is already there
+
+param.n_videos_in_batch = 4;
+param.write_orig = ~exist(saliency_flicker_writers{2}.txt, 'file');
+if ~param.write_orig
+    param.n_batches = 1;
+else
+    param.n_batches = 2;
+end
+param.n_output_videos = param.n_videos_in_batch * param.n_batches; %  to be verified in write_videos()
+
+video_writers = cell(1, param.n_output_videos);
+writable_imgs = cell(1, param.n_output_videos);
 % output video (modulated frames)
 video_writers{1} = prepare_video_writer(fullfile(output_video_path, [output_video_name '.avi']));
 % modulation saliency video
 video_writers{2} = prepare_video_writer(fullfile(output_video_path, [output_video_name ' saliency' '.avi']));
 % flicker saliency video
-video_writers{3} = prepare_video_writer(fullfile(output_video_path, [output_video_name ' flicker-saliency' '.avi']));
+video_writers{3} = prepare_video_writer(fullfile(output_video_path, [output_video_name ' saliency-flicker' '.avi']));
 % merged outputs of the above three
 video_writers{4} = prepare_video_writer(fullfile(output_video_path, [output_video_name ' all' '.avi']));
+
+if param.write_orig
+    video_writers{5} = prepare_video_writer(fullfile(output_video_path, 'orig.avi'));
+    video_writers{6} = prepare_video_writer(fullfile(output_video_path, ['saliency' '.avi']));
+    video_writers{7} = prepare_video_writer(fullfile(output_video_path, ['saliency-flicker' '.avi']));
+    video_writers{8} = prepare_video_writer(fullfile(output_video_path, ['all' '.avi']));
+end
 
 fprintf('\nframe \tmax ROI \tmean diff \n');
 fprintf('001');
@@ -146,17 +165,18 @@ diffs = make_diffs(pyrasBef);
 % make mask
 % SBef = simple_n(enlarge(get_salimap(pyrasBef)));
 [SBef, S_flicker] = get_salimap(pyrasBef);
-[SBef, S_flicker] = process_saliency(SBef, S_flicker);
+[writable_imgs{6}, writable_imgs{7}] = process_saliency(SBef, S_flicker);
 % fprintf('#1 mean saliency (normalised, upscaled):%f\n',mean(SBef(:)));
 % fprintf('#1 mean flicker saliency (as is):%f\n',mean(S_flicker(:)));
-avg_flicker_saliency=zeros(length(tld.source.idx),1);
-avg_flicker_saliency(1)=mean(S_flicker(:));
-mask = simple_n(get_video_mask(curBB) .* SBef);
+
+saliency_flicker_writers{2}.avg = zeros(length(tld.source.idx), 1);
+saliency_flicker_writers{2}.avg(1) = mean(writable_imgs{7}(:));
+mask = simple_n(get_video_mask(curBB) .* writable_imgs{6});
 
 % do enhancement
 [param.ehcBc, param.ehcBd] = init_betas(curFrame, mask);
 % TODO(zori) figure out how modu_1st_frame is different from modu_frame and if I can merge them
-[editedFrame, W(:,:,1)] = modu_1st_frame(curFrame, diffs, mask, SBef);
+[editedFrame, W(:,:,1)] = modu_1st_frame(curFrame, diffs, mask, writable_imgs{6});
 
 [flash, W] = store_and_shift(flash, W, curFrame, diffs, mask, curBB);
 
@@ -164,6 +184,10 @@ mask = simple_n(get_video_mask(curBB) .* SBef);
 % re-generation of pyras
 pyrasAft = make_pyras(editedFrame);
 pyras_modulated = pyrasAft;
+[s_modulated, s_flicker_modulated] = get_salimap(pyras_modulated);
+[writable_imgs{2}, writable_imgs{3}] = process_saliency(s_modulated, s_flicker_modulated);
+saliency_flicker_writers{1}.avg = zeros(length(tld.source.idx), 1);
+saliency_flicker_writers{1}.avg(1) = mean(writable_imgs{3}(:));
 % calculate gamma
 rgbDiff = abs(editedFrame - curFrame);
 gamma = (1 - mean(rgbDiff(:)));
@@ -197,11 +221,11 @@ for i = 2:length(tld.source.idx) % for every frame
         % make mask
         % SBef = simple_n(enlarge(get_salimap(pyrasBef)));
         [SBef, S_flicker] = get_salimap(pyrasBef); % TODO(zori) this is flicker BEFORE; would be the same regardless of the modulation type
-        [SBef, S_flicker] = process_saliency(SBef, S_flicker);
+        [writable_imgs{6}, writable_imgs{7}] = process_saliency(SBef, S_flicker);
         %         fprintf('#2 mean saliency (normalised, upscaled):%f\n',mean(SBef(:)));
         %         fprintf('#2 mean flicker saliency (as is):%f\n',mean(S_flicker(:)));
-        avg_flicker_saliency(i)=mean(S_flicker(:));
-        mask = simple_n(get_video_mask(curBB) .* SBef);
+        saliency_flicker_writers{2}.avg(i) = mean(writable_imgs{7}(:));
+        mask = simple_n(get_video_mask(curBB) .* writable_imgs{6});
 
         % do enhancement
         [editedFrame, W(:,:,1)] = ...
@@ -221,26 +245,34 @@ for i = 2:length(tld.source.idx) % for every frame
     end
     
     if i == param.flashL
-        [input_img, output_img, meanW, weights, weightsIdx] = modulate_frame(flash, W, weights, weightsIdx);
+        [input_img, writable_imgs{1}, meanW, weights, weightsIdx] = modulate_frame(flash, W, weights, weightsIdx);
 
         if TO_VISUALIZE
-            visualHandles = init_visual(fig_h, input_img, output_img, [], [], meanW, ...
+            % % save the image of first frame with the bounding box in the
+            % % original video's folder (only once)
+            % ff = figure('visible', 'off'); imshow(input_img, 'Border', 'tight');
+            % BB = flash.curBB{1}; rectangle('Position', [BB(1) BB(2) BB(3)-BB(1) BB(4)-BB(2)], 'LineWidth', 4, 'EdgeColor', 'y');
+            % print(ff, '-r80', '-djpeg', fullfile(source.input, [opt.source.init_bb_name(1:end-4) '.jpg']));
+
+            visualHandles = init_visual(fig_h, input_img, writable_imgs{1}, [], [], meanW, ...
                 flash.curBB{1});
         end
         % SAft = simple_n(enlarge(get_salimap(pyrasAft)));
         
         
     elseif i > param.flashL
-        [input_img, output_img, meanW, weights, weightsIdx] = modulate_frame(flash, W, weights, weightsIdx);
+        [input_img, writable_imgs{1}, meanW, weights, weightsIdx] = modulate_frame(flash, W, weights, weightsIdx);
 
         if TO_VISUALIZE
-            visualHandles = visualize(fig_h, input_img, output_img, [], [], meanW, ...
+            visualHandles = visualize(fig_h, input_img, writable_imgs{1}, [], [], meanW, ...
                 flash.curBB{1}, visualHandles);
         end
-        pyras_modulated = make_pyras(output_img, pyras_modulated);
-        [s_modulated, s_flicker_modulated] = get_salimap(pyras_modulated); % TODO(zori) this is flicker BEFORE; would be the same regardless of the modulation type
-        [s_modulated, s_flicker_modulated] = process_saliency(s_modulated, s_flicker_modulated);
-        write_videos(video_writers, output_img, s_modulated, s_flicker_modulated);
+        pyras_modulated = make_pyras(writable_imgs{1}, pyras_modulated);
+        [s_modulated, s_flicker_modulated] = get_salimap(pyras_modulated);
+        [writable_imgs{2}, writable_imgs{3}] = process_saliency(s_modulated, s_flicker_modulated);
+        saliency_flicker_writers{1}.avg(i) = mean(writable_imgs{3}(:));
+        if param.write_orig, writable_imgs{5} = input_img; end
+        write_videos(video_writers, writable_imgs);
     end
     
     %---SALIENCY-END---
@@ -256,7 +288,7 @@ for i = 2:length(tld.source.idx) % for every frame
         
         %%
         %--SALIENCY-BEGIN--
-        on_finish(video_writers);        
+        on_finish(video_writers, saliency_flicker_writers);
         
         %---SALIENCY-END---
         %%
@@ -278,19 +310,21 @@ end
 for i = 1:param.flashL
     [flash, W] = shift_storage(flash, W);
     
-    [input_img, output_img, meanW, weights, weightsIdx] = modulate_frame(flash, W, weights, weightsIdx);
+    [input_img, writable_imgs{1}, meanW, weights, weightsIdx] = modulate_frame(flash, W, weights, weightsIdx);
 
     if TO_VISUALIZE
-        visualHandles = visualize(fig_h, input_img, output_img, [], [], meanW, ...
+        visualHandles = visualize(fig_h, input_img, writable_imgs{1}, [], [], meanW, ...
             flash.curBB{1}, visualHandles);
     end
-    pyras_modulated = make_pyras(output_img, pyras_modulated);
-    [s_modulated, s_flicker_modulated] = get_salimap(pyras_modulated); % TODO(zori) this is flicker BEFORE; would be the same regardless of the modulation type
-    [s_modulated, s_flicker_modulated] = process_saliency(s_modulated, s_flicker_modulated);
-    write_videos(video_writers, output_img, s_modulated, s_flicker_modulated);
+    pyras_modulated = make_pyras(writable_imgs{1}, pyras_modulated);
+    [s_modulated, s_flicker_modulated] = get_salimap(pyras_modulated);
+    [writable_imgs{2}, writable_imgs{3}] = process_saliency(s_modulated, s_flicker_modulated);
+    saliency_flicker_writers{1}.avg(i) = mean(writable_imgs{3}(:));
+    if param.write_orig, writable_imgs{5} = input_img; end
+    write_videos(video_writers, writable_imgs);
 end
 
-on_finish(video_writers);
+on_finish(video_writers, saliency_flicker_writers);
 
 %---SALIENCY-END---
 %%
@@ -359,9 +393,6 @@ switch param.modulation_type
         % with values 1/(param.wSpan)
     case MOD_TYPE_EXP_SMOOTHING
         W_reshaped = reshape(W, param.nEhcMaps * param.MAGICAL_DIMENSION, param.wSpan);
-        % TODO(zori) get the alpha, make sure it is set only once, and the
-        % smoothing param is available for the name of the video in the
-        % beginning
         assert(isfield(param, 'alpha'));
         res = W_reshaped * param.alpha;
         meanW = reshape(res, param.nEhcMaps, param.MAGICAL_DIMENSION);
@@ -379,31 +410,47 @@ weightsIdx = weightsIdx + 1;
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function write_videos(video_writers, output_img, saliency, flicker_s)
-global n_output_videos;
-assert(length(video_writers) == n_output_videos);
-writeVideo(video_writers{1}, output_img);
-writeVideo(video_writers{2}, saliency); % saliency frame
-writeVideo(video_writers{3}, flicker_s); % flicker saliency frame
-
-cc = stitch_outputs(output_img, saliency, flicker_s); % stitch the three images together
-writeVideo(video_writers{4}, cc); % the big video containing all channels
+function write_videos(video_writers, writable_imgs)
+global param;
+assert(length(video_writers) == param.n_output_videos);
+for k = 1:param.n_batches
+    l = (k-1) * param.n_videos_in_batch + 2;
+    stitched = stitch_outputs(writable_imgs{l-1}, writable_imgs{l}, writable_imgs{l+1}); % stitch the three images together
+    writable_imgs{k * param.n_videos_in_batch} = stitched; % the big video containing all channels
+end
+write_videos_do(video_writers, writable_imgs);
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function stitched = stitch_outputs(output_img,SBef,S_flicker)
-stitched = cat(1, output_img, repmat(SBef, [1 1 3]), repmat(S_flicker, [1 1 3]));
+function write_videos_do(video_writers, writable_imgs)
+global param;
+% assert(length(writable_imgs) == param.n_output_videos);
+for k = 1:param.n_output_videos
+    writeVideo(video_writers{k}, writable_imgs{k});
+end
+% writeVideo(video_writers{1}, output_img);
+% writeVideo(video_writers{2}, saliency); % saliency frame
+% writeVideo(video_writers{3}, flicker_s); % flicker saliency frame
+% cc = stitch_outputs(output_img, saliency, flicker_s); % stitch the three images together
+% writeVideo(video_writers{4}, cc); % the big video containing all channels
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% function on_finish(videoWriterObj,videoSaliencyWriterObj,...
-%     videoFlickerSaliencyWriterObj,videosWO,output_flicker_saliency_txt,avg_flicker_saliency)
-function on_finish(video_writers)
-for k = 1:length(video_writers)
+function stitched = stitch_outputs(output_img, saliency, saliency_flicker)
+% stitch_outputs(output_img,SBef,S_flicker)
+stitched = cat(1, output_img, repmat(saliency, [1 1 3]), repmat(saliency_flicker, [1 1 3]));
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function on_finish(video_writers, saliency_flicker_writers)
+global param;
+assert(length(video_writers) == param.n_output_videos);
+for k = 1:param.n_output_videos
     close(video_writers{k});
 end
-
-% dlmwrite(output_flicker_saliency_txt,mean(avg_flicker_saliency),'delimiter',' ');
-% dlmwrite(output_flicker_saliency_txt,avg_flicker_saliency','-append',...
-%     'roffset',1,'delimiter',' ');
+for k = 1:param.n_batches
+    dlmwrite(saliency_flicker_writers{k}.txt, mean(saliency_flicker_writers{k}.avg), 'delimiter', ' ');
+    dlmwrite(saliency_flicker_writers{k}.txt, saliency_flicker_writers{k}.avg', ...
+        '-append', 'roffset', 1, 'delimiter', ' ');
+end
 end
