b6d3e44a24b363b1c75a331988dc1d69c610c92b
Zori Kostadinova <zornitsa.kostadinova.bg@gmail.com>
2015-06-08 13:51:37 +0900

    Added screenshots of the target ROI in the first frame of the video sequences, of the 4 experiments that I will regularly use to the dataset.

--- video_modulation/TLD_source/img/img_dir.m
+++ video_modulation/TLD_source/img/img_dir.m
@@ -8,7 +8,10 @@ function images = img_dir(path)
 % Looks into 'path' directory and returns a list of images in it
 
 % considered extensions
-ext = {'*.jpeg','*.jpg','*.png','*.pgm'};
+% ext = {'*.jpeg','*.jpg','*.png','*.pgm'};
+% UPDATE(zori) only consider .png, as I keep the image of first frame with the
+% bounding box in a .jpg
+ext = {'*.png'};
 
 images = [];
 for i = 1:length(ext)
