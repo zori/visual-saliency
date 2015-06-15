f2981fbc59cf895d02779a12d6f15d72f7d269f4
Zori Kostadinova <zornitsa.kostadinova.bg@gmail.com>
2015-06-15 19:16:30 +0900

    Changed the inner loops with the outer, so that a sequence completes entirely (all ROIs) before we proceed to the next one. (allows for an interrupted experiment to be rerun easily with run_TLD or run_all, adjusting sequence_names.

--- video_modulation/run/run_all.m
+++ video_modulation/run/run_all.m
@@ -3,7 +3,7 @@
 % 8.5.0.197613 (R2015a)
 % based on run_TLD
 
-% sequence_names={
+% sequence_names = {
 %     'etdb_HallMonitor_cif_300',...
 %     'etdb_HARBOUR_352x288_30_300',...
 %     'etdb_MOBILE_352x288_30_300',...
@@ -15,7 +15,7 @@
 
 % EDIT(zori) I will only work with these two sequences, as ROIs can be chosen so
 % that the tracker doesn't lose the object until the end of the video.
-sequence_names={
+sequence_names = {
     'orig_city_300',... % init1 (tower_bell_orig; smaller); init2 (init_1C - main building part; larger)
     'etdb_MOBILE_352x288_30_300',... % init1 (pigs; larger); init2 (yellow goat head; smaller)
     };
@@ -24,28 +24,25 @@ sequence_names={
 % 1) no modulation (would result in flicker)
 % 2) video modulation with simple moving average (as Tao SHI originally implemented)
 % 3) exponential smoothing with smoothing weight alpha
-modulation=struct('m',{1,2,3,3},'s',{NaN, NaN,3,7});
+modulation = struct('m',{1,2,3,3},'s',{NaN, NaN,3,7});
 
 % file with initial bounding box (ROI)
-% initial_regions={'init1.txt'};
-% initial_regions={'init2.txt'};
-initial_regions={'init1.txt','init2.txt'};
+% initial_regions = {'init1.txt'};
+% initial_regions = {'init2.txt'};
+initial_regions = {'init1.txt','init2.txt'};
 
-for ir=1:length(initial_regions)
-    init_workspace;
-    % opt=struct;
-    for n = 1:length(sequence_names)
+for n = 1:length(sequence_names)
+    for ir = 1:length(initial_regions)
         init_workspace;
         opt.sequence_name = sequence_names{n};
         for k=1:4
-                % opt.source          = struct('camera',0,'input',fullfile('dataset','video',opt.sequence_name,filesep),'bb0',[]);
                 opt.source = struct('init_bb_name',initial_regions{ir},...
                     'camera',0,...
                     'input',fullfile('dataset','video',opt.sequence_name,filesep),...
                     'bb0',[]); % camera/directory swith, directory_name, initial_bounding_box (if empty, it will be selected by the user)
-                opt.modulation=modulation(k).m;
-            opt.smoothing_param_recipr=modulation(k).s;
-            opt=run_all_set_opt(opt);
+                opt.modulation = modulation(k).m;
+            opt.smoothing_param_recipr = modulation(k).s;
+            opt = run_all_set_opt(opt);
             [bb,conf,weights] = tldSaliency(opt);
         end
     end
