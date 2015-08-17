48292814e4afa1f684c5fff803122ff2a194e4b8
Zori Kostadinova <zornitsa.kostadinova.bg@gmail.com>
2015-08-17 16:36:30 +0900

    Added T_SLLS enumeration type for the within-frame WLLS minimisation. For reproducibility, leaving the T_WLLS, despite wrong weights for this problem.

--- video_modulation/auxiliary/MinimisationOption.m
+++ video_modulation/auxiliary/MinimisationOption.m
@@ -2,16 +2,20 @@
 % Aug 2015
 % 8.5.0.197613 (R2015a)
 classdef MinimisationOption
-    enumeration % in-frame type of minimisation (if any)
-        T_ORIG % original code, as Tao SHI implemented it, no minimisation
-        T_LLS % non-weighted linear least squares
-        T_WLLS % weighted LLS
-    end
-    enumeration % area to minimise on
-        A_ROI
+    % area to minimise over
+    enumeration
+        A_ROI % region of interest
         A_IMG % entire image
     end
-    enumeration % temporal (between this and the previous frame) type of minimisation (if any)
+    % within-frame type of minimisation
+    enumeration
+        T_ORIG % original code, as Tao SHI implemented it, no minimisation
+        T_LLS % non-weighted linear least squares
+        T_WLLS % flicker-saliency weighted LLS; note these are the wrong weights to use for this minimisation, but keep the code for reproducibility
+        T_SLLS % modulation-saliency weighted LLS; these should be the correct weighting for this problem
+    end
+    % temporal (between this and the previous frame) type of minimisation
+    enumeration
         TEMP_O % original code, as Tao SHI implemented it, no minimisation
         TEMP_L % LLS
         TEMP_W % weighted LLS
