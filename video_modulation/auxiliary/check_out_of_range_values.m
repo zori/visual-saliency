55ca35fbfd958ba976552136267ab3715f84b8e6
Zori Kostadinova <zornitsa.kostadinova.bg@gmail.com>
2015-08-28 15:58:00 +0900

    Code to check (and optionally, fix the output values of a matrix into [0, 1]. Loggs out-of-range values and number of frames. To be used for a modulated RGB image.

---
+++ video_modulation/auxiliary/check_out_of_range_values.m
@@ -0,0 +1,22 @@
+% Zornitsa Kostadinova
+% Aug 2015
+% 8.5.0.197613 (R2015a)
+function [min_pix, max_pix, num_out_of_range, frame_out] = check_out_of_range_values(frame_out)
+global param;
+frame_out_v = frame_out(:);
+min_pix = min(frame_out_v);
+max_pix = max(frame_out_v);
+
+num_out_of_range = sum(sum(frame_out_v < 0 | frame_out_v > 1));
+if min_pix < 0 || max_pix > 1
+    assert(num_out_of_range ~= 0)
+    warning(['out-of-range value(s) - min: ' num2str(min_pix) '; max: ' num2str(max_pix)]);
+
+    param.frames_out_of_range_modulated_vals = param.frames_out_of_range_modulated_vals + 1;
+    param.num_out_of_range_modulated_vals = param.num_out_of_range_modulated_vals + num_out_of_range;
+
+    % hedge the output to [0, 1]
+    frame_out(frame_out < 0) = 0;
+    frame_out(frame_out > 1) = 1;
+end
+end
