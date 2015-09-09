a3495a4a094e56c7ec6312f7eaebf85e25cd1e45
Zori Kostadinova <zornitsa.kostadinova.bg@gmail.com>
2015-09-09 17:35:30 +0900

    Renamed the file that used to provide the old dataset (most videos are not suitable for our experiment, as the camera movement is very rapid and TLD tracker cannot track any object for the full length of the video seuqence).

--- video_modulation/frames2video.m
+++ video_modulation/frames2video.m
@@ -1,5 +1,5 @@
 function frames2video()
-video_sequence=video_dataset_data();
+video_sequence = video_dataset_data_old();
 for k=1:length(video_sequence)
     video_folder = fullfile('dataset', 'video', [video_sequence(k).name '_' int2str(video_sequence(k).n_frames)]);
     single_frames2video(video_folder, video_sequence(k).n_frames);
