857bf0c9df936effd81b7319b47826acccc9a319
Zori Kostadinova <zornitsa.kostadinova.bg@gmail.com>
2015-09-11 14:32:31 +0900

    Unfinished script to analyse experimental data for statistical significance.

---
+++ video_modulation/auxiliary/test_student_t.py
@@ -0,0 +1,15 @@
+import scipy.stats
+import numpy as np
+
+# fix seed to get the same result as examples on the internet
+# np.random.seed(12345678)
+
+# t : float or array - The calculated t-statistic
+# prob : float or array - The two-tailed p-value
+
+# means of the statistics
+# TODO: finish script for student_t analysis
+tao = [
+zori = 
+# axis : int, optional - Axis can equal None (ravel array first), or an integer (the axis over which to operate on a and b)
+[t, prob] = scipy.stats.ttest_ind(tao, zori, axis=0)
